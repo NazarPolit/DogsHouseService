436317a4997b8f5c0badea337d619c81a7450896
Nazar <politovskyinazar@gmail.com>
2025-11-01 19:07:13 +0200

    Implement /ping endpoint

---
+++ DogsHouseService.API/Controllers/PingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DogsHouseService.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PingController : ControllerBase
+	{
+		[HttpGet]
+		[Route("/ping")]
+		public IActionResult GetPing()
+		{
+			return Ok("Dogshouseservice.Version1.0.1");
+		}
+	}
+}
