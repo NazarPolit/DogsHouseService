76ad201c3a2e4c6da8aa5b23616921efeb77e867
Nazar <politovskyinazar@gmail.com>
2025-11-03 16:12:40 +0200

    Unit tests for PingController added

---
+++ DogsHouseService.Tests/PingControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using DogsHouseService.API.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogsHouseService.Tests
+{
+	public class PingControllerTests
+	{
+		[Fact]
+		public void GetPing_WhenCalled_ReturnsCorrectString()
+		{
+			var controller = new PingController();
+
+			var result = controller.GetPing();
+
+			var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+
+			okResult.Value.Should().Be("Dogshouseservice.Version1.0.1");
+		}
+	}
+}
